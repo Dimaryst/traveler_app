ddca19942f31f39adbcbad29f5c72f7a605a49af
Dimaryst <dimaryst@icloud.com>
2022-03-30 19:10:14 +0300

    UI updated

--- rpi_traveler_app/rpi_traveler_app/ContentView.swift
+++ rpi_traveler_app/rpi_traveler_app/ContentView.swift
@@ -12,16 +12,41 @@ struct ContentView: View {
     @State var networks: [Network] = []
     var body: some View {
         NavigationView {
-            List (networks)
-            { nw in
-                Text(nw.ssid)
-            }
-            .onAppear {
-                Api().getNetworks { (networks) in
-                    self.networks = networks
+            VStack(alignment: .leading) {
+                List {
+                    Section(header: Text("Available Networks")){
+                        ForEach(networks, id: \.id) {
+                            nw in
+                            HStack{
+                                Label("", systemImage: "\(encryptionTypeToImage(encryptionType: nw.encryption))")
+                                Text(nw.ssid)
+                            }
+                                }
+                            }
+                        }
+            // .listStyle(GroupedListStyle())
+            .navigationTitle("Pi Traveler")
+            .toolbar {
+                ToolbarItem {
+                    Button(action: getNetworks) {
+                        Label("Scan", systemImage: "arrow.clockwise")
+                    }
                 }
             }
         }
+        }
+    }
+    
+    private func getNetworks() {
+        networks = []
+        Api().getNetworks { (networks) in
+            self.networks = networks
+        }
+    }
+    private func encryptionTypeToImage(encryptionType: String) -> String {
+        var img = "lock.open"
+        if encryptionType != "off" {img = "lock"}
+        return img
     }
 }
     
--- rpi_traveler_app/rpi_traveler_app/Data.swift
+++ rpi_traveler_app/rpi_traveler_app/Data.swift
@@ -11,12 +11,15 @@ struct Network: Codable, Identifiable {
     let id = UUID()
     var ssid: String
     var mac: String
+    var signal_quality: Int32
+    var encryption: String
 }
 
 class Api {
     func getNetworks(completion: @escaping ([Network]) -> ()) {
         guard let url = URL(string: "http://192.168.26.1:5000/api/available_networks") else { return }
         URLSession.shared.dataTask(with: url) { data, _, _ in
+            
             let networks = try! JSONDecoder().decode([Network].self, from: data!)
             DispatchQueue.main.async {
                 completion(networks)
